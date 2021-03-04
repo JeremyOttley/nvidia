baaceeb3509ef1806bb5266d23e5e70b2302c4d6
Jeremy Ottley <Gazaunga@gmail.com>
2021-03-04 12:39:18 -0500

    Update functions.lua

--- awesome/functions.lua
+++ awesome/functions.lua
@@ -2,3 +2,18 @@ function twitch_stream()
   channel = io.read() -- twitch_stream("varsitygaming")
   return os.execute("streamlink --twitch-low-latency " .. "https://www.twitch.tv/" .. channel .. " best")
 end
+
+-- two versions
+
+abyss = " >/dev/null 2>&1 &"
+
+function interactive_launch()
+  input = io.read()
+  os.execute("google-chrome --app=https://" .. input .. abyss)
+end
+
+function launch(url)
+  os.execute("google-chrome --app=https://" .. url .. abyss)
+end
+
+-- launch("twitch.tv/varsitygaming")
