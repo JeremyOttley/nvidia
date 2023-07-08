c4892c619bca0dd301778f32ecafe3332eb68298
Jeremy Ottley <Gazaunga@gmail.com>
2023-07-08 19:37:50 -0400

    Update xmonad.hs

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -20,6 +20,10 @@ myEditor :: String
 myEditor = "emacsclient -c -a 'emacs' "  -- Sets emacs as editor
 -- myEditor = myTerminal ++ " -e vim "    -- Sets vim as editor
 
+myStartupHook :: X ()
+myStartupHook = do
+  spawn "picom -cfF -o 0.38 -O 200 -I 200 -t 0 -l 0 -r 3 -D2 -m 0.88"
+
 main :: IO ()
 main = xmonad desktopConfig
 	{ modMask = myModMask
