e08bae23aac249c8c4ad542625eed2d08de35add
Jeremy Ottley <Gazaunga@gmail.com>
2023-07-08 19:02:58 -0400

    Create xmonad.hs

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,27 @@
+import XMonad
+import XMonad.Config.Desktop
+
+myFont :: String
+myFont = "Noto Sans 11"
+
+myModMask :: KeyMask
+myModMask = mod4Mask        -- Sets modkey to super/windows key
+
+myTerminal :: String
+myTerminal = "alacritty"    -- Sets default terminal
+
+myBrowser :: String
+myBrowser = "qutebrowser "  -- Sets qutebrowser as browser
+
+myEmacs :: String
+myEmacs = "emacsclient -c -a 'emacs' "  -- Makes emacs keybindings easier to type
+
+myEditor :: String
+myEditor = "emacsclient -c -a 'emacs' "  -- Sets emacs as editor
+-- myEditor = myTerminal ++ " -e vim "    -- Sets vim as editor
+
+main :: IO ()
+main = xmonad desktopConfig
+	{ modMask = myModMask
+	, terminal = myTerminal
+	}
